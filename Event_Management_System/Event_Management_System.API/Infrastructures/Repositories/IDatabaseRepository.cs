344fc3fd99d05ebf3bb97303f7eb1bd01c1af918
Promise30 <promiseadeagbo@gmail.com>
2025-11-12 17:37:34 +0100

    Task: Added database repository files for pagination related properties

---
+++ Event_Management_System/Event_Management_System.API/Infrastructures/Repositories/IDatabaseRepository.cs
@@ -0,0 +1,18 @@
+﻿using Event_Management_System.API.Domain.DTOs;
+using Event_Management_System.API.Domain.Entities;
+using Event_Management_System.API.Helpers;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Linq.Expressions;
+
+namespace Event_Management_System.API.Infrastructures.Repositories
+{
+    public interface IDatabaseRepository<T, Tkey> where T : BaseEntity<Tkey>
+    {
+        Task<PagedList<T>> GetAllPaginatedAsync(RequestParameters parameter, Expression<Func<T, bool>> filter = null);
+
+        Task<PagedList<T>> GetAllPaginatedAsync(RequestParameters parameter, SortParameters sortParams, Expression<Func<T, bool>> filter = null);
+        Task<PagedList<T>> GetAllPaginatedAsync(RequestParameters parameter, List<T> entity, Expression<Func<T, bool>> filter = null);
+
+
+    }
+}
